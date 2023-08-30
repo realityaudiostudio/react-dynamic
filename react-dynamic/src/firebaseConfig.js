b38010d1179cb2822e122c064591c104d84071ba
Alan Jose Santo <realityaudiostudio@gmail.com>
2023-08-30 23:33:10 +0530

    angane vijayich

---
+++ react-dynamic/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZeUTunDTAbWB_4xPBCsrDJGhVEGXKtR4",
+    authDomain: "dynamic-ajs.firebaseapp.com",
+    projectId: "dynamic-ajs",
+    storageBucket: "dynamic-ajs.appspot.com",
+    messagingSenderId: "686203870080",
+    appId: "1:686203870080:web:9ec6ccd32c78e40866bf65",
+    measurementId: "G-6RW5PHXMB4"
+  };
+  
+  export default firebaseConfig;
